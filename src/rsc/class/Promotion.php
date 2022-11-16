7c090041d43279b7a0a702c844277441d6876627
Ahmed Fakhfakh <fakhfakhahmed45@gmail.com>
2022-11-16 15:05:57 +0100

    Lier la classe Promotion avec les autres classes Etudiant Et Controle

--- src/rsc/class/Promotion.php
+++ src/rsc/class/Promotion.php
@@ -4,6 +4,8 @@ class Promotion
     // Variables
 
     private $nom;
+    private $mesControles = array();
+    private $mesEtudiants = array();
 
     // Encapsulation
 
@@ -15,6 +17,30 @@ class Promotion
     public function setNom($nouveauNom)
     {
         $this->nom=$nouveauNom;
+    }
+
+    public function ajouterControle($unControle)
+    {
+        array_push($this->mesControles, $unControle);
+    }
+
+    public function supprimerControle($unControle)
+    {
+        if (array_key_exists($unControle, $this->mesControles)) {
+            unset($this->mesControles[$unControle]);
+        }
+    }
+
+    public function ajouterEtudiant($unEtudiant)
+    {
+        array_push($this->mesEtudiants, $unEtudiant);
+    }
+
+    public function supprimerEtudiant($unEtudiant)
+    {
+        if (array_key_exists($unEtudiant, $this->mesEtudiants)) {
+            unset($this->mesEtudiants[$unEtudiant]);
+        }
     }
 }
 ?>
