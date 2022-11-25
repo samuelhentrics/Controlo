c6a450252d53114a5bb5ef9ee1a43aa56f8336d8
Samuel HENTRICS <samuel.hentrics@gmail.com>
2022-11-25 20:08:36 +0100

    Le fichier config.php a désormais une fonction qui permet de retrouver le chemin du dossier du projet par défaut, peut importe où celui-ci est executé.

--- src/config.php
+++ src/config.php
@@ -1,6 +1,28 @@
 <?php
 
-$PATH = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER["REQUEST_URI"];
+const NOM_DOSSIER_PROJET = "src";
+
+function retrouverCheminApp(){
+    // On récupére le chemin du fichier où est lancé le script
+    $cheminFichierLance = explode('/', $_SERVER["REQUEST_URI"], -1);
+
+    // Parcourir le tableau de la fin jusqu'à trouver le nom du dossier où se trouve l'application
+    // cela permet ainsi d'éviter s'il existe plusieurs fichiers du même nom de les confondre
+    $numRechercheSrc=count($cheminFichierLance)-1;
+    while ($cheminFichierLance[$numRechercheSrc] != NOM_DOSSIER_PROJET) {
+        $numRechercheSrc--;
+    }
+
+    // On reconstitue le chemin de l'application
+    $cheminVersProjet = '';
+    for ($indiceReconstructionChemin=0; $indiceReconstructionChemin <= $numRechercheSrc; $indiceReconstructionChemin++) { 
+        $cheminVersProjet .= $cheminFichierLance[$indiceReconstructionChemin].'/';
+    }
+
+    return $cheminVersProjet;
+}
+
+$PATH = 'http://'.$_SERVER['HTTP_HOST'].retrouverCheminApp();
 
 //echo $PATH.$_SERVER['PHP_SELF'];
 
