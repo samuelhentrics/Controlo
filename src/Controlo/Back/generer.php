a9b191ce513bed2a9e8d1492c61916f3c4f1731c
Benjamin <=>
2022-12-12 13:24:14 +0100

    Debut recuperation avc id controle

--- src/Controlo/Back/generer.php
+++ src/Controlo/Back/generer.php
@@ -1,1 +1,10 @@
+<?php 
+include(FONCTION_CREER_LISTE_CONTROLES_PATH);
+
+$numControle = $_GET["numControle"];
+$leControle = creerListeControles()[$numControle];
+
+print_r($leControle->getNomLong());
+
+?>
 Non codé en cours
