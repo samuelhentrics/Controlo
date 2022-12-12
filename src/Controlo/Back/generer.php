23d97b342a9d4d2320d8148c3dc5a8ed5b6758d4
Benjamin <=>
2022-12-12 15:27:30 +0100

    Afficher Les salles ainsi que le nombre d'etudiants

--- src/Controlo/Back/generer.php
+++ src/Controlo/Back/generer.php
@@ -5,6 +5,9 @@ $numControle = $_GET["numControle"];
 $leControle = creerListeControles()[$numControle];
 
 print_r($leControle->getNomLong());
-
-?>
-Non codé en cours
+print("<br> -------nombre de salle pour ce controle: ".count($leControle->getMesSalles()) ."| nombre etudiants: ".count($leControle->getMesPromotions()[0]->getMesEtudiants())."--------");
+// var_dump($leControle->getMesPromotions()[0]->getMesEtudiants());
+foreach ($leControle->getMesSalles() as $key => $value) {
+    print("<br>".$value->getNom());
+}
+print("<br>---------------");
