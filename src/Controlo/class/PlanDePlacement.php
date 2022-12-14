addab8a83093e98aba71be8f63fd5c298ace595e
Ahmed Fakhfakh <fakhfakhahmed45@gmail.com>
2022-12-14 15:16:13 +0100

    Classe PlanDePlacement V1

---
+++ src/Controlo/class/PlanDePlacement.php
@@ -0,0 +1,37 @@
+<?php
+class PlanDePlacement{
+
+    private $maSalle;
+    private $monControle;
+    private $maContrainteGenerale;
+    private $maContrainteEspacement;
+
+    public function getMaSalle(){
+        return $this->maSalle;
+    }
+    public function setMaSalle($unesalle){
+        $this->maSalle = $uneSalle;
+    }
+
+    public function getMonControle(){
+        return $this->monControle;
+    }
+    public function setMonControle($unControle){
+        $this->monControle = $unControle;
+    }
+
+    public function getMaContrainteGenerale(){
+        return $this->maContrainteGenerale;
+    }
+    public function setMaContrainteGenerale($uneContrainteGenerale){
+        $this->maContrainteGenerale = $uneContrainteGenerale;
+    }
+
+    public function getMaContrainteEspacement(){
+        return $this->maContrainteEspacement;
+    }
+    public function setMaContrainteEspacement($uneContrainteEspacement){
+        $this->maContrainteEspacement = $uneContrainteEspacement;
+    }
+
+}
