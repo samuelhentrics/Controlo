8327dc0987f418fc22273117aba61abf38d0c861
° PesTo ° <etcheparecedric@gmail.com>
2023-01-05 22:33:45 +0100

    Suppréssion du mode développement dans resultat.php

--- src/Controlo/Back/resultat.php
+++ src/Controlo/Back/resultat.php
@@ -273,71 +273,7 @@ if (!$erreur) {
   print("Erreur lors du placement des étudiants<br><br>");
 }
 
-
-
-// ----------------------------------------------------------------
-// ----------------------------------------------------------------
-// Mode développement
-// ----------------------------------------------------------------
-// ----------------------------------------------------------------
-
-// $listeMesZones = $unControle->getMesSalles()["S124"]->getMonPlan()->getMesZones();
-// function nbPlaceDispo($listeMesZones)
-// {
-//   $returnArray = array();
-//   $totalPlace = 0;
-//   $totalPrise = 0;
-//   foreach ($listeMesZones as $uneZone) {
-//     foreach ($uneZone as $unePosition) {
-//       if ($unePosition->getType() === "place") {
-//         $totalPlace++;
-//       }
-//       if ($unePosition->getInfoPrise())
-//         $totalPrise = $totalPrise + 1;
-
-//     }
-//   }
-//   $returnArray["totalPrise"] = $totalPrise;
-//   $returnArray["totalPlace"] = $totalPlace;
-//   return $returnArray;
-// }
-
-// foreach ($unControle->getMesSalles() as $key => $uneSalle) {
-//   print($uneSalle->getNom() . " info: ");
-//   print_r(nbPlaceDispo($uneSalle->getMonPlan()->getMesZones()));
-//   print(" <br> ");
-// }
-// // var_dump($listeMesZones);
-
-// // print( . "<- nombre de place <br>");
-// //debut du placement
-// // print($uneSalle->getNom()." $unControle->getMesSalles()["S124"]->getMonPlan()->getMesZones() . "<br>");
-// foreach ($unControle->getMesSalles() as $nomSalle => $uneSalle) {
-//   for ($i = 1; $i < 21; $i++) {
-//     $unePlace = new Zone();
-//     $unePlace->setType("place");
-//     $unePlace->setNumero($i);
-
-//     $unEtudiant = new Etudiant("NOM" . $i, "PRENOM", 1, 2, "helloworld@gmail.com");
-
-//     $unPlacement = new UnPlacement();
-//     $unPlacement->setMonEtudiant($unEtudiant);
-//     $unPlacement->setMaZone($unePlace);
-
-//     $unPDP->ajouterPlacement($unPlacement);
-//     $unPDP->setMaSalle($uneSalle);
-//   }
-
-//   // echo "<h2>Génération des PDP en PDF</h2>";
-//   $unControle->ajouterPlanDePlacement($unPDP);
-//   // echo "ok";
-// }
-// // genererPDF($unControle);
-// //aaficher controle
-// // var_dump( $unControle);
-
-// print("contrle id: " . $_GET["id"] . "<br>")
-//   ?>
+   ?>
 <html>page resultat
 
 </html>
