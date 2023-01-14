463b2510d2091150c0eaeef62d8ea190d77401e6
Samuel HENTRICS <samuel.hentrics@gmail.com>
2023-01-14 14:15:37 +0100

    les ressources de fontAwesome sont maintenant utilisables sans connexion

--- src/Controlo/Back/footer.php
+++ src/Controlo/Back/footer.php
@@ -1,8 +1,18 @@
-<footer class="footer mt-auto py-3 bg-light">
-    <div class="container">
-        © 2022 Controlo |
-        <a class="text-reset fw-bold" href="#">Mentions Légales</a>
-        |
-        <a class="text-reset fw-bold" href="#">Politique de Confidentialité</a>
+<footer class="bg-light py-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-9">
+        <p class="text-muted">
+          © 2022 Controlo | 
+          <a class="text-reset" href="#">Mentions Légales</a> | 
+          <a class="text-reset" href="#">Politique de Confidentialité</a>
+        </p>
+      </div>
+      <div class="col-lg-3 d-flex justify-content-end">
+        <p class="text-muted text-right">
+          Version 1.0
+        </p>
+      </div>
     </div>
+  </div>
 </footer>
--- src/index.php
+++ src/index.php
@@ -10,20 +10,21 @@
         <!-- CSS -->
         <link href="<?php echo CSS_PATH; ?>bootstrap.min.css" rel="stylesheet">
         <link href="<?php echo CSS_PATH; ?>datatables.bootstrap5.css" rel="stylesheet">
+        <link href="<?php echo CSS_PATH; ?>fontawesome.css" rel="stylesheet">
+        <link href="<?php echo CSS_PATH; ?>brands.css" rel="stylesheet">
+        <link href="<?php echo CSS_PATH; ?>solid.css" rel="stylesheet">
 
         <!-- Javascript -->
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+        <script src="<?php echo JS_PATH; ?>popper.min.js"></script>
         <script src="<?php echo JS_PATH; ?>bootstrap.min.js"></script>
         <script src="<?php echo JS_PATH; ?>jquery.min.js"></script>
         <script src="<?php echo JS_PATH; ?>jquery.dataTables.min.js"></script>
-        <script src="<?php echo JS_PATH; ?>datatables.bootstrap5.js"></script>
-        <script src="https://kit.fontawesome.com/1c7be7f624.js" crossorigin="anonymous"></script>
-        
+        <script src="<?php echo JS_PATH; ?>datatables.bootstrap5.js"></script>       
 
     </head>
 
     <body>
-        <?php include(BACK_PATH."header.php"); ?>
+        <?php require(BACK_PATH."header.php"); ?>
 
 
         <?php
@@ -38,86 +39,62 @@
                 switch ($page) {
                     // Cas où l'utilisateur souhaite voir la liste des contrôles
                     case 'controles':
-                        include(BACK_PATH."controles.php");
+                        require(BACK_PATH."controles.php");
                         # code...
                         break;
 
                     case 'choixGeneration':
-                        include(BACK_PATH."choixGeneration.php");
+                        require(BACK_PATH."choixGeneration.php");
                         # code...
                         break;
 
 
                     // Cas où l'utilisateur souhaite voir la liste des étudiants
                     case 'etudiants':
-                        include(BACK_PATH."etudiants.php");
+                        require(BACK_PATH."etudiants.php");
                         break;
                     
                     // Cas où l'utilisateur souhaite voir la liste des salles
                     case 'salles':
                         if (isset($_GET['salle'])) {
-                            include(BACK_PATH."planSalle.php");
+                            require(BACK_PATH."planSalle.php");
                         }
                         // Ou plus précisement le plan d'une salle
                         else{
-                            include(BACK_PATH."salles.php");
+                            require(BACK_PATH."salles.php");
                         }
                         break;
 
                     // Mode développement
                     case 'test':
-                        include(BACK_PATH."test.php");
+                        require(BACK_PATH."test.php");
                         break;
 
                     // Générer les plans de placement
                     case 'generer':
-                        include(IMPORT_PATH."generer.php");
+                        require(IMPORT_PATH."generer.php");
                         break;
 
                     // Page de résultat de génération plans de placement
                     case 'resultat':
-                        include(BACK_PATH."resultat.php");
+                        require(BACK_PATH."resultat.php");
                         break;
 
-                    // Demande de l'utilisateur a télécharger les PDP
-                    case 'telechargement':
-                        if (isset($_GET['id'])){
-                            $id = $_GET['id'];
-                            // Rediriger vers la page de téléchargement
-                            echo "<meta http-equiv='refresh' content='0;url=download.php?id=$id'>";
-
-                            echo '<script>
-                            $(document).ready(function () {
-                                //Handler for .ready() called.
-                                window.setTimeout(function () {
-                                    location.href = "'.PAGE_RESULTAT_PATH.'&succes=ok&id='.$id.'";
-                                }, 500);
-                            });
-                            
-                            </script>';
-
-                        }
-                        else{
-                            // Rediriger vers la page d'accueil
-                            echo "<meta http-equiv='refresh' content='0;url=index.php'>";
-                        }
-                        
-                        break;
 
                     default:
                     // Cas où la demande est incorrecte, on retourne un message 404
-                        include(BACK_PATH."404.php");
+                        require(BACK_PATH."404.php");
                         break;
                 }
             }
             else 
             // Cas où l'utilisateur est sur la page d'accueil
-            include(BACK_PATH."accueil.php");
+            require(BACK_PATH."accueil.php");
 
 
         ?>
 
-        <?php include(BACK_PATH."footer.php"); ?>
+        <?php require(BACK_PATH."footer.php"); ?>
 
     </body>
 
