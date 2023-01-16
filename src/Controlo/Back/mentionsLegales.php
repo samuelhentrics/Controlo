d98d051e2548825b56e26bd042b751ffa5aebdc3
Cédric ETCHEPARE <90332714+CedricEtchepare@users.noreply.github.com>
2023-01-16 12:02:41 +0100

    Ajout de la politique de confidentialité et des mentions légales

---
+++ src/Controlo/Back/mentionsLegales.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <br>
+    <h1>Mentions Légales</h1> 
+    <br>
+    <p><strong>Identité éditeur du site : </strong>Cédric ETCHEPARE, Samuel HENTRICS LOISTINE, Ahmed FAKHFAKH, Benjamin PEYRE</p> 
+    <p><strong>Coordonnées : </strong>2 Allée du Parc Montaury, 64600 Anglet, contact@controlo-iut.fr</p> 
+    <p><strong>Mentions relatives à la propriété intellectuelle : </strong>Données manipulées par l’application issues du logiciel de scolarité de l’IUT de Bayonne mais filtrées pour éviter les données personnelles (INE, adresse…). Utilisation par le secrétariat du l’IUT qui détient les droits sur ces données.</p> 
+    <p><strong>Mentions relatives à l'hébergement du site : </strong>
+    <p><strong>Nom : </strong>OVH</p>
+    <p><strong>SAS au capital : </strong>10 174 560 €</p>
+    <p><strong>RCS : </strong>Lille Métropole 424 761 419 00045</p>
+    <p><strong>Code APE : </strong>2620Z</p>
+    <p><strong>N° TVA : </strong>FR 22 424 761 419</p>
+    <p><strong>Siège social : </strong>2 rue Kellermann - 59100 Roubaix - France</p>
+    <p><strong>Président : </strong>Michel Paulin</p>
+    <p><strong>N° TVA : </strong>FR 22 424 761 419</p>
+    <p>OVH SAS est une filiale de la société OVH Groupe SA, société immatriculée au RCS de Lille sous le numéro 537 407 926 sise 2, rue Kellermann, 59100 Roubaix.
+</p>
+    </div> 
---
+++ src/Controlo/Back/politiqueDeConfidentialite.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <br>
+    <h1>Politique de confidentialité</h1> 
+    <br>
+    <p><strong>Identité responsable traitement :</strong> Cédric ETCHEPARE, Samuel HENTRICS LOISTINE, Ahmed FAKHFAKH, Benjamin PEYRE</p> 
+    <p><strong>Coordonnées :</strong> 2 Allée du Parc Montaury, 64600 Anglet, contact@controlo-iut.fr</p> 
+    <p><strong>Données collectées :</strong> Données manipulées par l’application issues du logiciel de scolarité de l’IUT de Bayonne mais filtrées pour éviter les données personnelles (INE, adresse…). Utilisation par le secrétariat du l’IUT qui détient les droits sur ces données.</p> 
+    <p><strong>Manière de collecte :</strong> Le secrétariat choisit les fichiers CSV qu’il veut importer.</p> 
+    <p><strong>Finalité et durée de conservation des données :</strong> Dans le cas où les données ne sont pas traitées depuis 3 ans, on a le devoir de les supprimer. Bien évidemment, le secrétariat peut les supprimer de lui-même à tout moment.</p> 
+    <p><strong>Destinataire des données :</strong> La base de données</p> 
+    <p><strong>Droits d’accès / Modification :</strong> Seul le secrétariat qui utilise notre application a le droit de modification sur les données. En revanche les créateurs de la base de données peuvent le consulter et le supprimer en cas d’inactivité de 3 ans par exemple.</p> 
+    <p><strong>Transfert hors union européenne :</strong> Non</p> 
+    <p><strong>Cookies :</strong> Non</p> 
+    <p>Il est également recommandé de rappeler aux utilisateurs que les éléments composant le site web bénéficient de la protection par un droit de propriété intellectuelle et que toute copie, reproduction et ou utilisation de ce contenu sans l’accord de leur auteur constitue une atteinte aux droits de l’auteur du site web passible de sanctions.</p> 
+    </div> 
