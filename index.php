34ab6bbe5878f07819bf5b8024f34f64bf8dec72
Samuel HENTRICS <samuel.hentrics@gmail.com>
2023-01-18 21:59:40 +0100

    Ajout d'une vidéo explicative pour comment générer les PDF, ajout d'un index qui redirige l'utilisateur selon ses choix

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+    <html lang="fr">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Controlo</title>
+
+        <!-- CSS -->
+        <link href="src/Controlo/Front/css/bootstrap.min.css" rel="stylesheet">
+        <link href="src/Controlo/Front/css/datatables.bootstrap5.css" rel="stylesheet">
+        <link href="src/Controlo/Front/css/fontawesome.css" rel="stylesheet">
+        <link href="src/Controlo/Front/css/brands.css" rel="stylesheet">
+        <link href="src/Controlo/Front/css/solid.css" rel="stylesheet">
+
+        <!-- Javascript -->
+        <script src="src/Controlo/Front/js/popper.min.js"></script>
+        <script src="src/Controlo/Front/js/bootstrap.min.js"></script>
+        <script src="src/Controlo/Front/js/bootstrap.bundle.min.js"></script>
+        <script src="src/Controlo/Front/js/jquery.min.js"></script>
+        <script src="src/Controlo/Front/js/jquery.dataTables.min.js"></script>
+        <script src="src/Controlo/Front/js/datatables.bootstrap5.js"></script>       
+
+    </head>
+
+    <body>
+        <main>
+            <div class="alert alert-warning" role="alert">
+                Les documents de la partie "Algorithme et Diagramme UML" ne sont pas à jour. Ils seront bientôt mis à jour.
+            </div>
+            <div class="card-group">
+                <div class="card">
+                    <img class="card-img-top" src="src/Controlo/Front/images/banniere2.png" alt="Ressources SAE">
+                    <div class="card-body">
+                    <h5 class="card-title">Ressources demandées pour la SAE</h5>
+                    <p class="card-text">En cliquant sur les liens, vous accéderez aux ressources demandées pour la SAE.</p>
+                    <p class="card-text">
+                        <a href="Spécification" class="btn btn-primary">Spécifications Externes</a><br><br>
+                        <a href="Algorithme" class="btn btn-primary">Algorithme et Diagramme UML</a><br><br>
+                        <a href="docs" class="btn btn-primary">Doxygen</a><br><br>
+                    </p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="src/Controlo/Front/images/banniere.png" alt="Logo de Controlo">
+                    <div class="card-body">
+                    <h5 class="card-title">Accéder à Controlo</h5>
+                    <p class="card-text">
+                        <a href="src" class="btn btn-primary">Découvrir</a>
+                    </p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="src/Controlo/Front/images/banniere3.png" alt="Manuel Utilisateur">
+                    <div class="card-body">
+                    <h5 class="card-title">Manuel Utilisateur</h5>
+                    <p class="card-text">Familiarisez-vous avec Controlo grâce à notre Manuel Utilisateur.</p>
+                    <p class="card-text">
+                        <a href="src/index.php?page=manuelUtilisateur" class="btn btn-primary">Accéder</a>
+                    </p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+
+    </html>
