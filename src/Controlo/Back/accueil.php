68b8467b4792e86d09b065d8cc41783de46ea26c
BenjaminPeyre <bpeyre@univ-pau.fr>
2023-01-20 00:08:30 +0100

    Nouvelle page d'accueil type flip card

--- src/Controlo/Back/accueil.php
+++ src/Controlo/Back/accueil.php
@@ -3,21 +3,18 @@
     <h1 class="mt-5">Bienvenue sur Controlo</h1>
     <p>Bienvenue sur Controlo, cette plateforme permet de générer des plans de placement pour des
       contrôles.</p>
+    <div class="row text-center">
+      <div class="col-md-4 card-container">
+        <div class="card card-flip">
+          <div class="front card-block">
+            <!-- <span class="card-img-top fa" style="font-size: 4em">&#xf118;</span> -->
+            <h4 class="card-title">Controles</h4>
+            <h6 class="card-subtitle text-muted">Emplacement du dossier</h6>
+            <p class="card-text"><?php echo CSV_CONTROLES_PATH; ?></p>
+          </div>
+          <div class="back card-block">
 
-    <div id="accordion" class="card-deck" style="display: flex">
-      <div class="card mx-5">
-        <div class="card-header" id="headingOne">
-          <h5 class="mb-0">
-            <button class="btn btn-link">
-              <!-- Le dossier /<?php echo CSV_CONTROLES_FOLDER_NAME; ?> -->
-              Controles
-            </button>
-          </h5>
-        </div>
-        <div id="controle" aria-labelledby="controle">
-          <div class="card-body">
-            Contenu actuel du dossier
-            <br>
+            <p> Contenu actuel du dossier </p>
             <?php
             $directory = CSV_CONTROLES_FOLDER_NAME;
             $files = scandir($directory);
@@ -26,31 +23,25 @@
             foreach ($files as $file) {
               $extension = pathinfo($file, PATHINFO_EXTENSION);
               if ($extension === "csv")
-                echo "<br>" . $file . "<br>";
+                echo $file . "<br>";
             }
-            ?>
-          </div>
-
-          <div class="card-body-desc">
-            Le fichier <?php echo LISTE_CONTROLES_FILE_NAME; ?> contient la liste des contrôles.
+            ?></p>
+            <br>
+            <!-- <a href="#" class="btn btn-outline-primary">Read More</a> -->
           </div>
         </div>
       </div>
+      <div class="col-md-4 card-container">
+        <div class="card card-flip">
+          <div class="front card-block">
+            <!-- <span class="card-img-top fa" style="font-size: 4em">&#xf118;</span> -->
+            <h4 class="card-title">Étudiants</h4>
+            <h6 class="card-subtitle text-muted">Emplacement du dossier</h6>
+            <p class="card-text"><?php echo CSV_ETUDIANTS_PATH; ?></p>
+          </div>
+          <div class="back card-block">
 
-
-      <div class="card mx-5">
-        <div class="card-header" id="headingTwo">
-          <h5 class="mb-0">
-            <button class="btn btn-link">
-              <!-- Le dossier /<?php echo CSV_ETUDIANTS_FOLDER_NAME; ?> -->
-              ETUDIANTS
-            </button>
-          </h5>
-        </div>
-        <div id="etudiant" aria-labelledby="headingTwo" data-parent="#accordion">
-          <div class="card-body">
-            Contenu actuel du dossier
-            <br>
+            <p> Contenu actuel du dossier </p>
             <?php
             $directory = CSV_ETUDIANTS_FOLDER_NAME;
             $files = scandir($directory);
@@ -61,30 +52,23 @@
               if ($extension === "csv")
                 echo $file . "<br>";
             }
-            echo "</p>";
-            ?>
-          </div>
-          <div class="card-body-desc">
-            Chaque fichier CSV contiennent une promotion (le nom de la promotion est celle du fichier).
+            ?></p>
+            <br>
+            <!-- <a href="#" class="btn btn-outline-primary">Read More</a> -->
           </div>
         </div>
       </div>
+      <div class="col-md-4 card-container">
+        <div class="card card-flip">
+          <div class="front card-block">
+            <!-- <span class="card-img-top fa" style="font-size: 4em">&#xf118;</span> -->
+            <h4 class="card-title">Salles</h4>
+            <h6 class="card-subtitle text-muted">Emplacement du dossier</h6>
+            <p class="card-text"><?php echo CSV_SALLES_PATH; ?></p>
+          </div>
+          <div class="back card-block">
 
-
-
-      <div class="card mx-5">
-        <div class="card-header" id="headingThree">
-          <h5 class="mb-0">
-            <button class="btn btn-link">
-              <!-- Le dossier /<?php echo CSV_SALLES_FOLDER_NAME; ?> -->
-              Salles
-            </button>
-          </h5>
-        </div>
-        <div id="salle" aria-labelledby="headingThree" data-parent="#accordion">
-          <div class="card-body">
-            Contenu actuel du dossier
-            <br>
+            <p> Contenu actuel du dossier </p>
             <?php
             $directory = CSV_SALLES_FOLDER_NAME;
             $files = scandir($directory);
@@ -95,29 +79,89 @@
               if ($extension === "csv")
                 echo $file . "<br>";
             }
-            ?>
-          </div>
-          <div class="card-body-desc">
-            Le fichier <?php echo LISTE_SALLES_FILE_NAME; ?> contient la liste des salles ainsi que la salle voisine
-            d'une salle si celle-ci existe.<br>
-            Chaque fichier CSV contiennt le plan d'une salle (le nom la salle est celle du fichier).
+            ?></p>
+            <br>
+            <!-- <a href="#" class="btn btn-outline-primary">Read More</a> -->
           </div>
         </div>
       </div>
-
-
-
     </div>
-    <script>
-$(document).ready(function(){
-  $(".card-header").mouseenter(function(){
-    $(this).parent()[0].querySelector('.card-body-desc').hidden = false
-  });
-  $(".card-header").mouseleave(function(){
-    $(this).parent()[0].querySelector('.card-body-desc').hidden = true
-  });
-});
-</script>
-
   </div>
 </main>
+
+<style>
+  .card-block .btn-outline-primary {
+    width: 100%;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+    bottom: 0;
+    left: 0;
+    position: absolute;
+  }
+
+  .card {
+    margin: 20px 0;
+  }
+
+  /* Flip Cards CSS */
+  .card-container {
+    perspective: 700px;
+  }
+
+  .card-flip {
+    position: relative;
+    width: 100%;
+    transform-style: preserve-3d;
+    height: auto;
+    transition: all 0.5s ease-out;
+    background: white;
+    border: none;
+  }
+
+  .card-flip div {
+    backface-visibility: hidden;
+    transform-style: preserve-3d;
+    height: 100%;
+    width: 100%;
+    border: none;
+  }
+
+  .card-flip .front {
+    position: relative;
+    z-index: 1;
+  }
+
+  .card-flip .back {
+    position: relative;
+    z-index: 0;
+    transform: rotateY(-180deg);
+  }
+
+  .card-container:hover .card-flip {
+    transform: rotateY(180deg);
+  }
+</style>
+<script>
+  $(document).ready(function() {
+    var front = document.getElementsByClassName("front");
+    var back = document.getElementsByClassName("back");
+
+    var highest = 0;
+    var absoluteSide = "";
+
+    for (var i = 0; i < front.length; i++) {
+      if (front[i].offsetHeight > back[i].offsetHeight) {
+        if (front[i].offsetHeight > highest) {
+          highest = front[i].offsetHeight;
+          absoluteSide = ".front";
+        }
+      } else if (back[i].offsetHeight > highest) {
+        highest = back[i].offsetHeight;
+        absoluteSide = ".back";
+      }
+    }
+    $(".front").css("height", highest);
+    $(".back").css("height", highest);
+    $(absoluteSide).css("position", "absolute");
+  });
+</script>
