178a1fcaa807c7db398cec28dc7061c246e66585
Samuel HENTRICS <samuel.hentrics@gmail.com>
2023-02-03 09:44:29 +0100

    Ajouter étudiant suite

--- src/Controlo/import/CRUD/etudiantsCRUD.php
+++ src/Controlo/import/CRUD/etudiantsCRUD.php
@@ -22,31 +22,112 @@
  * @throws Exception Si le fichier CSV ne contient pas les champs obligatoires de la nomenclature
  * @return bool Retourne vrai si l'ajout s'est bien passé, faux sinon
  */
-function ajouterEtudiant($nomEtudiant, $prenomEtudiant, $nomPromotion,
-$tdEtudiant, $tpEtudiant, $emailEtudiant, $tiersTempsEtudiant,
-$ordinateurEtudiant, $demissionnaireEtudiant){
+function ajouterEtudiant(
+    $nomEtudiant,
+    $prenomEtudiant,
+    $nomPromotion,
+    $tdEtudiant,
+    $tpEtudiant,
+    $emailEtudiant,
+    $tiersTempsEtudiant,
+    $ordinateurEtudiant,
+    $demissionnaireEtudiant
+)
+{
 
     // On initialise un booléen en cas d'erreur
     $ajoutOk = true;
 
     // Tentative d'écriture du fichier CSV
-    try
-    {
+    try {
+        $lienFichier = CSV_ETUDIANTS_FOLDER_NAME . $nomPromotion . ".csv";
+
+        // Récupérer un array selon l'entete du fichier CSV
+        $infoEtudiant = recupererEnteteEtudiant($lienFichier);
+
+        // Faire un array avec les informations de l'étudiant
+        $infoEtudiant = saisirLigneEtudiant(
+            $infoEtudiant,
+            $nomEtudiant,
+            $prenomEtudiant,
+            $tdEtudiant,
+            $tpEtudiant,
+            $emailEtudiant,
+            $tiersTempsEtudiant,
+            $ordinateurEtudiant,
+            $demissionnaireEtudiant
+        );
+
+
+        // Ajouter l'étudiant en fin de fichier
+
+        // Ouvrir le fichier CSV en mode écriture en fin de fichier
+        $monFichier = fopen($lienFichier, "a");
+
+        if (!$monFichier) {
+            throw new Exception("Impossible d'ouvrir le fichier CSV. Il semble être ouvert par un autre programme.");
+        }
+
+        // Ajouter l'array dans le CSV
+        fputcsv($monFichier, $infoEtudiant, ";");
+
+        // Fermer le fichier CSV
+        fclose($monFichier);
+
+    } catch (Exception $e) {
+        throw new Exception($e->getMessage());
+    }
+
+    return $ajoutOk;
+}
+
+function supprimerEtudiant($nomPromotion, $idEtudiant)
+{
+
+    // On initialise un booléen en cas d'erreur
+    $suppressionOk = true;
+
+    // Tentative de suppression de l'étudiant
+    try {
         $lienFichier = CSV_ETUDIANTS_FOLDER_NAME . $nomPromotion . ".csv";
 
         // Ouvrir le fichier CSV en mode lecture pour récupérer les entêtes
+
+
+
+
+
+    } catch (Exception $e) {
+        // Afficher l'erreur      
+        echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">';
+        echo $e->getMessage();
+        echo "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>";
+        echo '</div>';
+
+        $suppressionOk = false;
+    }
+}
+
+/**
+ * @brief Retourne un array prêt à remplir avec pour clés les entêtes du fichier CSV
+ * @param string $lienFichier Chemin du fichier CSV
+ * @throws Exception 
+ * @return array|null Retourne un array prêt à remplir avec pour clés les entêtes du fichier CSV, null si le fichier CSV ne peut être ouvert
+ */
+function recupererEnteteEtudiant($lienFichier)
+{
+    try {
+        // Ouvrir le fichier CSV en mode lecture pour récupérer les entêtes
         $monFichier = fopen($lienFichier, "r");
 
-        if($monFichier === false)
-        {
+        if ($monFichier === false) {
             throw new Exception("Impossible d'ouvrir le fichier CSV");
         }
 
         // Récupérer les entêtes
         $entetes = fgetcsv($monFichier, 10000, ";");
 
-        if($entetes === false)
-        {
+        if ($entetes === false) {
             throw new Exception("Impossible de récupérer les entêtes du fichier CSV");
         }
 
@@ -57,73 +138,86 @@ $ordinateurEtudiant, $demissionnaireEtudiant){
         $entetes = array_map('trim', $entetes);
 
         // Vérifier que l'entete contient les champs obligatoires de la nomenclature
-        if (!in_array(NOM_NOM_COLONNE_ETUDIANT, $entetes) || !in_array(PRENOM_NOM_COLONNE_ETUDIANT, $entetes) ||
+        if (
+            !in_array(NOM_NOM_COLONNE_ETUDIANT, $entetes) || !in_array(PRENOM_NOM_COLONNE_ETUDIANT, $entetes) ||
             !in_array(TD_NOM_COLONNE_ETUDIANT, $entetes) || !in_array(TP_NOM_COLONNE_ETUDIANT, $entetes) ||
-            !in_array(MAIL_NOM_COLONNE_ETUDIANT, $entetes) || !in_array(STATUTS_NOM_COLONNE_ETUDIANT, $entetes))
-        {
+            !in_array(MAIL_NOM_COLONNE_ETUDIANT, $entetes) || !in_array(STATUTS_NOM_COLONNE_ETUDIANT, $entetes)
+        ) {
             throw new Exception("Le fichier CSV de la promotion ne contient pas les champs obligatoires de la nomenclature");
         }
 
         // Mettre les valeurs de l'entete en tant que clés
         $infoEtudiant = array_flip($entetes);
-        foreach ($infoEtudiant as $key => $value)
-        {
+        foreach ($infoEtudiant as $key => $value) {
             $infoEtudiant[$key] = null;
         }
 
+        return $infoEtudiant;
+    } catch (Exception $e) {
+        throw $e;
+    }
+}
+
+/**
+ * @brief Modifie une ligne d'un array d'une info étudiante
+ * @param array $infoEtudiant Array contenant les infos d'un étudiant
+ * @param string $nomEtudiant Nom de l'étudiant
+ * @param string $prenomEtudiant Prénom de l'étudiant
+ * @param int $tdEtudiant Numéro du TD de l'étudiant
+ * @param int $tpEtudiant Numéro du TP de l'étudiant
+ * @param string $emailEtudiant Email de l'étudiant
+ * @param string $tiersTempsEtudiant Tiers temps de l'étudiant
+ * @param string $ordinateurEtudiant Ordinateur de l'étudiant
+ * @param string $demissionnaireEtudiant Démisionnaire de l'étudiant
+ * @return array Liste contenant les infos d'un étudiant
+ */
+function saisirLigneEtudiant($infoEtudiant, $nomEtudiant, $prenomEtudiant, $tdEtudiant, $tpEtudiant, $emailEtudiant, $tiersTempsEtudiant, $ordinateurEtudiant, $demissionnaireEtudiant)
+{
+    try {
         // Créer les données à écrire dans le fichier CSV
         $infoEtudiant[NOM_NOM_COLONNE_ETUDIANT] = $nomEtudiant;
         $infoEtudiant[PRENOM_NOM_COLONNE_ETUDIANT] = $prenomEtudiant;
+
+        // Vérifier que le numéro de TD est un entier
+        if (!is_numeric($tdEtudiant)) {
+            throw new Exception("Le numéro de TD de l'étudiant n'est pas un entier");
+        }
         $infoEtudiant[TD_NOM_COLONNE_ETUDIANT] = $tdEtudiant;
+
+        // Vérifier que le numéro de TP est un entier
+        if (!is_numeric($tpEtudiant)) {
+            throw new Exception("Le numéro de TP de l'étudiant n'est pas un entier");
+        }
         $infoEtudiant[TP_NOM_COLONNE_ETUDIANT] = $tpEtudiant;
+
+        // Vérifier que le mail est valide
+        if (!filter_var($infoEtudiant[MAIL_NOM_COLONNE_ETUDIANT], FILTER_VALIDATE_EMAIL)) {
+            throw new Exception("Le mail de l'étudiant n'est pas valide");
+        }
+
         $infoEtudiant[MAIL_NOM_COLONNE_ETUDIANT] = $emailEtudiant;
 
         $infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT] = null;
-        if ($tiersTempsEtudiant == "on")
-        {
-            $infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT].= "Tiers temps";
+        if ($tiersTempsEtudiant == "on") {
+            $infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT] .= "Tiers temps";
         }
-        
-        if ($ordinateurEtudiant == "on")
-        {
-            if ($infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT] != null)
-            {
-                $infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT].= ", ";
+
+        if ($ordinateurEtudiant == "on") {
+            if ($infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT] != null) {
+                $infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT] .= ", ";
             }
-            $infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT].= "Ordinateur";
+            $infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT] .= "Ordinateur";
         }
 
-        if ($demissionnaireEtudiant == "on")
-        {
-            if ($infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT] != null)
-            {
-                $infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT].= ", ";
+        if ($demissionnaireEtudiant == "on") {
+            if ($infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT] != null) {
+                $infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT] .= ", ";
             }
-            $infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT].= "Démisionnaire";
+            $infoEtudiant[STATUTS_NOM_COLONNE_ETUDIANT] .= "Démissionnaire";
         }
-
-        // Ajouter l'étudiant en fin de fichier
-
-        // Ouvrir le fichier CSV en mode écriture en fin de fichier
-        $monFichier = fopen($lienFichier, "a");
-
-        // Ajouter l'array dans le CSV
-        fputcsv($monFichier, $infoEtudiant, ";");
-
-        // Fermer le fichier CSV
-        fclose($monFichier);
-
     }
-    catch (Exception $e)
-    {
-        // Afficher l'erreur      
-        echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">';
-        echo $e->getMessage();
-        echo "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>";
-        echo '</div>';
-
-        $ajoutOk = false;
+    catch (Exception $e) {
+        throw $e;
     }
-
-    return $ajoutOk;
+    return $infoEtudiant;
 }
