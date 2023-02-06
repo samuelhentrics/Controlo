6fed74d166516717e6a5d286445e4e8bcbb92c22
Ahmed Fakhfakh <fakhfakhahmed45@gmail.com>
2023-02-06 14:34:32 +0100

    Création du tableau pour créer le plan de placement

--- src/Controlo/Back/Salles/creerPlanDeSalle.php
+++ src/Controlo/Back/Salles/creerPlanDeSalle.php
@@ -3,6 +3,17 @@ $nomSalle = $_POST['nomSalle'];
 $salleVoisine = $_POST['salleVoisine'];
 $nbrLigne = $_POST['nbrLigne'];
 $nbrColonne = $_POST['nbrColonne'];
-
-
 ?>
+<table border="1">
+  <?php for ($i = 0; $i < $nbrLigne; $i++) { ?>
+    <tr>
+      <?php for ($j = 0; $j < $nbrColonne; $j++) { ?>
+        <td><input type="text" name="<?php echo $i . '-' . $j; ?>"></td>
+      <?php } ?>
+    </tr>
+  <?php } ?>
+</table>
+
+
+
+
