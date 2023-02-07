ad883f17cbf3c039a8735bebd6b8fb2c6a9ec309
Samuel HENTRICS <samuel.hentrics@gmail.com>
2023-02-07 21:02:28 +0100

    Mise à jour de la page d'accueil et de l'année du projet (footer)

--- index.php
+++ index.php
@@ -1,65 +1,63 @@
 <!doctype html>
-    <html lang="fr">
+<html lang="fr">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Controlo</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Controlo</title>
 
-        <!-- CSS -->
-        <link href="src/Controlo/Front/css/bootstrap.min.css" rel="stylesheet">
-        <link href="src/Controlo/Front/css/datatables.bootstrap5.css" rel="stylesheet">
-        <link href="src/Controlo/Front/css/fontawesome.css" rel="stylesheet">
-        <link href="src/Controlo/Front/css/brands.css" rel="stylesheet">
-        <link href="src/Controlo/Front/css/solid.css" rel="stylesheet">
+    <!-- CSS -->
+    <link href="src/Controlo/Front/css/bootstrap.min.css" rel="stylesheet">
+    <link href="src/Controlo/Front/css/datatables.bootstrap5.css" rel="stylesheet">
+    <link href="src/Controlo/Front/css/fontawesome.css" rel="stylesheet">
+    <link href="src/Controlo/Front/css/brands.css" rel="stylesheet">
+    <link href="src/Controlo/Front/css/solid.css" rel="stylesheet">
 
-        <!-- Javascript -->
-        <script src="src/Controlo/Front/js/popper.min.js"></script>
-        <script src="src/Controlo/Front/js/bootstrap.min.js"></script>
-        <script src="src/Controlo/Front/js/bootstrap.bundle.min.js"></script>
-        <script src="src/Controlo/Front/js/jquery.min.js"></script>
-        <script src="src/Controlo/Front/js/jquery.dataTables.min.js"></script>
-        <script src="src/Controlo/Front/js/datatables.bootstrap5.js"></script>       
-
-    </head>
-
-    <body>
-        <main>
-
-            <div class="card-group">
-                <div class="card">
-                    <img class="card-img-top" src="src/Controlo/Front/images/banniere2.png" alt="Ressources SAE">
-                    <div class="card-body">
+<body>
+    <main>
+        <div class="card-group">
+            <div class="card">
+                <img class="card-img-top" src="src/Controlo/Front/images/banniere2.png" alt="Ressources SAE">
+                <div class="card-body">
                     <h5 class="card-title">Ressources demandées pour la SAE</h5>
-                    <p class="card-text">En cliquant sur les liens, vous accéderez aux ressources demandées pour la SAE.</p>
+                    <p class="card-text">En cliquant sur les liens, vous accéderez aux ressources demandées pour la SAE.
+                    </p>
                     <p class="card-text">
                         <a href="Spécification" class="btn btn-primary">Spécifications Externes</a><br><br>
                         <a href="Algorithme" class="btn btn-primary">Algorithme et Diagramme UML</a><br><br>
                         <a href="docs" class="btn btn-primary">Doxygen</a><br><br>
                     </p>
-                    </div>
                 </div>
-                <div class="card">
-                    <img class="card-img-top" src="src/Controlo/Front/images/banniere.png" alt="Logo de Controlo">
-                    <div class="card-body">
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="src/Controlo/Front/images/banniere.png" alt="Logo de Controlo">
+                <div class="card-body">
                     <h5 class="card-title">Accéder à Controlo</h5>
                     <p class="card-text">
                         <a href="src" class="btn btn-primary">Découvrir</a>
                     </p>
-                    </div>
                 </div>
-                <div class="card">
-                    <img class="card-img-top" src="src/Controlo/Front/images/banniere3.png" alt="Manuel Utilisateur">
-                    <div class="card-body">
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="src/Controlo/Front/images/banniere3.png" alt="Manuel Utilisateur">
+                <div class="card-body">
                     <h5 class="card-title">Manuel Utilisateur</h5>
                     <p class="card-text">Familiarisez-vous avec Controlo grâce à notre Manuel Utilisateur.</p>
                     <p class="card-text">
                         <a href="src/index.php?page=manuelUtilisateur" class="btn btn-primary">Accéder</a>
                     </p>
-                    </div>
                 </div>
             </div>
-        </main>
-    </body>
+        </div>
+    </main>
 
-    </html>
+    <!-- Javascript -->
+    <script src="src/Controlo/Front/js/popper.min.js"></script>
+    <script src="src/Controlo/Front/js/bootstrap.min.js"></script>
+    <script src="src/Controlo/Front/js/bootstrap.bundle.min.js"></script>
+    <script src="src/Controlo/Front/js/jquery.min.js"></script>
+    <script src="src/Controlo/Front/js/jquery.dataTables.min.js"></script>
+    <script src="src/Controlo/Front/js/datatables.bootstrap5.js"></script>
+</body>
+
+</html>
