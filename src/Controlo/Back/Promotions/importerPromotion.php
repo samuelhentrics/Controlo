44b62ef39c332f8c01c7ea45b7f44e3f85c68813
Samuel HENTRICS <samuel.hentrics@gmail.com>
2023-02-09 13:48:33 +0100

    Correction bogue importer

--- src/Controlo/Back/Promotions/importerPromotion.php
+++ src/Controlo/Back/Promotions/importerPromotion.php
@@ -3,9 +3,14 @@
     <div class="col-6 m-auto text-center">
         <?php
         include_once(FONCTION_CRUD_PROMOTIONS_PATH);
+        include_once(CLASS_PATH . CLASS_PROMOTION_FILE_NAME);
 
         try {
             if (isset($_FILES['fichierPromotion'])) {
+
+                // Récupération du nom de la promotion pour génération et pour l'affichage
+                $nomGeneration = $_POST["nomGeneration"];
+                $nomFormation = $_POST["nomFormation"];
 
                 // Récupération du nom du fichier de promotion
                 $fichierPromotion = $_FILES['fichierPromotion']['name'];
@@ -16,12 +21,7 @@
                 // Récupération du chemin temporaire du fichier sur le serveur
                 $cheminTemporaire = $_FILES['fichierPromotion']['tmp_name'];
 
-                // Définission du chemin où on souhaite enregistrer l'image
-                $emplacement = CSV_ETUDIANTS_FOLDER_NAME . $_FILES['fichierPromotion']['name'];
 
-                // Récupération du nom de la promotion pour génération et pour l'affichage
-                $nomGeneration = $_POST["nomGeneration"];
-                $nomFormation = $_POST["nomFormation"];
 
                 //Vérification de l'extension CSV
                 $extension = pathinfo($fichierPromotion, PATHINFO_EXTENSION);
@@ -29,13 +29,26 @@
                     throw new Exception("Le fichier importé n'est pas au format 'csv'");
                 }
 
-                // Emplacement du fichier avec le nouveau nom
-                $emplacementRenomme = CSV_ETUDIANTS_FOLDER_NAME . $nomGeneration . ".csv";
+                
+                // Définission du chemin où on souhaite enregistrer l'image
+                if ($nomGeneration != ""){
+                    $emplacement = CSV_ETUDIANTS_FOLDER_NAME . $nomGeneration . ".csv";
+                }
+                else{
+                    $emplacement = CSV_ETUDIANTS_FOLDER_NAME . $_FILES['fichierPromotion']['name'];
+                }
+
+                
+                if (file_exists($emplacement)) {
+                    throw new Exception("La promotion existe déjà. Veuillez la renommer ou la supprimer.");
+                }
+
+                move_uploaded_file($cheminTemporaire, $emplacement);
+
 
                 // Cas où seulement le fichier est saisi
                 if ($nomGeneration == ""& $nomFormation == "") {
                     // Déplacement de l'image du répertoire temporaire vers le répertoire de destination
-                    move_uploaded_file($cheminTemporaire, $emplacement);
                     $unePromotion = new Promotion($fichierPromotionSansExtension, $fichierPromotionSansExtension);
                     ajouterAffichagePromotion($unePromotion);
 
@@ -43,38 +56,30 @@
 
                 // Cas où le nom de génération et le fichier sont remplis
                 else if ($nomGeneration != ""& $nomFormation == "") {
-
                     // Déplacement de l'image du répertoire temporaire vers le répertoire de destination
-                    move_uploaded_file($cheminTemporaire, $emplacement);
-                    rename($emplacement, $emplacementRenomme);
                     $unePromotion = new Promotion($nomGeneration, $nomGeneration);
                     ajouterAffichagePromotion($unePromotion);
                 }
 
                 // Cas où le nom de formation et le fichier sont remplis
                 else if ($nomGeneration == ""& $nomFormation != "") {
-                    move_uploaded_file($cheminTemporaire, $emplacement);
                     $unePromotion = new Promotion($fichierPromotionSansExtension, $nomFormation);
                     ajouterAffichagePromotion($unePromotion);
                 }
 
                 // Cas où les 2 champs et le fichier sont remplis 
                 else {
-                    move_uploaded_file($cheminTemporaire, $emplacement);
-                    rename($emplacement, $emplacementRenomme);
                     $unePromotion = new Promotion($nomGeneration, $nomFormation);
                     ajouterAffichagePromotion($unePromotion);
                 }
 
                 echo '<div class="alert alert-success" role="alert">La promotion a bien été ajoutée</div>';
             }
-        }
-            
-            catch (Exception $e) {
-                echo '<div class="alert alert-danger" role="alert">
+        } catch (Exception $e) {
+            echo '<div class="alert alert-danger" role="alert">
                 Erreur lors de l\'ajout de la promotion : ' .
                 $e->getMessage() . '</div>';
-            }
+        }
         ?>
 
 
@@ -86,7 +91,8 @@
                 <label for="nom" class="col-4 col-form-label">Nom de promotion pour génération</label>
                 <div class="col-8">
                     <div class="input-group">
-                        <input id="controleNomLong" name="nomGeneration" placeholder="Ex : Info semestre 1" type="text" class="form-control">
+                        <input id="controleNomLong" name="nomGeneration" placeholder="Ex : Info semestre 1" type="text"
+                            class="form-control">
                     </div>
                 </div>
             </div>
@@ -94,7 +100,8 @@
                 <label for="controleNomCourt" class="col-4 col-form-label">Nom de promotion pour affichage</label>
                 <div class="col-8">
                     <div class="input-group">
-                        <input id="controleNomCourt" name="nomFormation" placeholder="Ex: BUT Informatique S1" type="text" class="form-control">
+                        <input id="controleNomCourt" name="nomFormation" placeholder="Ex: BUT Informatique S1"
+                            type="text" class="form-control">
                     </div>
                 </div>
             </div>
